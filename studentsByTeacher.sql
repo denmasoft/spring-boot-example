c4dec44978667b8aa9b989fa8f49b6b547fb01f2
DenmaSoft <denmasoft@gmail.com>
2019-11-13 16:33:27 -0500

    Create Sql scripts

--- studentsByTeacher.sql
+++ studentsByTeacher.sql
@@ -1,5 +1,6 @@
-SELECT t.id,t.age, t.name,t.sex,c.name as teacher from student t
+SELECT c.name as teacher, t.id,t.age, t.name,t.sex from student t
 inner join student_subjects st on st.student_id=t.id
 inner join subject b on b.id=st.subject_id
 inner join teacher c on b.teacher=c.id
 group by t.id, c.name
+order by c.name ASC
--- teachersByStudent.sql
+++ teachersByStudent.sql
@@ -1,5 +1,5 @@
-SELECT t.dni, t.name, d.name as student from teacher t
+SELECT d.name as student, t.dni, t.name from teacher t
 inner join subject s on s.teacher=t.id
 inner join student_subjects st on st.subject_id=s.id
 inner join student d on st.student_id=d.id
-group by t.dni,t.name,d.name
+group by d.name,t.dni,t.name
